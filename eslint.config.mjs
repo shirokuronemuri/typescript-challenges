a68d14ac3466289bf55485de027e3e711fe06532
Ihor Pavliuk <akira.pavl@gmail.com>
2025-03-11 15:36:25 +0200

    project setup, messing with eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    markdown: false,
+  },
+  {
+    rules: {
+      'import/first': 'off',
+      'no-alert': 'off',
+      'no-unused-expressions': 'off',
+      'no-unused-vars': 'off',
+      'no-use-before-define': 'off',
+      'prefer-const': 'off',
+      'ts/ban-ts-comment': 'off',
+      'ts/ban-types': 'off',
+      'ts/consistent-type-definitions': 'off',
+      'ts/no-namespace': 'off',
+      'ts/no-require-imports': 'off',
+      'ts/no-unused-vars': 'off',
+      'unused-imports/no-unused-vars': 'off',
+      'style/semi': ["error", "always"]
+    },
+  },
+)
